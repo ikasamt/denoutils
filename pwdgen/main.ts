8f32a73ca2ed7963b7104b8a76de882af6031ce2
ikasamt <ikasamt@gmail.com>
2023-02-02 18:03:56 +0900

    change size feature

--- pwdgen/main.ts
+++ pwdgen/main.ts
@@ -1,3 +1,11 @@
 import {gen} from "./mods.ts"
 
-console.log(gen(10))
+import { parse } from "https://deno.land/std@0.175.0/flags/mod.ts";
+
+const flags = parse(Deno.args, {
+    string: "size",
+    default: { size: "32" },
+});
+
+// deno run pwdgen/main.ts --size 12
+console.log(gen(parseInt(flags.size)))

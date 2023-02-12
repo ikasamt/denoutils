2e8c87591d896a77f174ff73cdae4a4ff7d939d1
ikasamt <ikasamt@gmail.com>
2023-02-12 11:17:02 +0900

    Update main.ts

--- pwdgen/main.ts
+++ pwdgen/main.ts
@@ -1,11 +1,11 @@
-import {gen} from "./mods.ts"
+import { gen } from "./mods.ts";
 
 import { parse } from "https://deno.land/std@0.175.0/flags/mod.ts";
 
 const flags = parse(Deno.args, {
-    string: "size",
-    default: { size: "32" },
+  string: "size",
+  default: { size: "32" },
 });
 
 // deno run pwdgen/main.ts --size 12
-console.log(gen(parseInt(flags.size)))
+console.log(gen(parseInt(flags.size)));
